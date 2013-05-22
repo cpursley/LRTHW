808c059e6336d356e1284d97f50a1c203635bee1
Chase Pursley <chasepursley@gmail.com>
2013-05-22 01:17:51 +0000

    completed exercises 6 - 10

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Learn Ruby The Hard Way
 ========
 
+Exercises from *Learn Ruby The Hard Way* by Zed Shaw
+
 http://ruby.learncodethehardway.org/book/
---
+++ ex02.rb
@@ -0,0 +1,7 @@
+# Comments And Pound Characters
+
+puts "I could have code like this." # and the comment after is 
+
+# puts "This won't run
+
+puts "This will run"
